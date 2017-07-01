f19a852df75573faa37ec8a5123b13d300105cf6
cocoapi <eksrns22tp@gmail.com>
2017-07-01 19:39:31 +0900

    include dx11 header

--- TMSG/TMSG/stdafx.h
+++ TMSG/TMSG/stdafx.h
@@ -10,6 +10,10 @@
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일:
 #include <windows.h>
+#include <d3d11.h>
+#include <d3d11sdklayers.h>
+#include <d3d11shader.h>
+#include <d3d11shadertracing.h>
 
 // C 런타임 헤더 파일입니다.
 #include <stdlib.h>
